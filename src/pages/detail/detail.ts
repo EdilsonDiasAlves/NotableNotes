c314adae330f35010eb0bd967a0ecbe14862f5d9
Edilson Dias <edilsondiasalves@hotmail.com>
2018-06-18 01:17:19 -0300

    Adding update form on details page

--- src/pages/detail/detail.ts
+++ src/pages/detail/detail.ts
@@ -1,13 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
-/**
- * Generated class for the DetailPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-detail',
@@ -19,7 +12,6 @@ export class DetailPage {
 
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.note = this.navParams.get('noteParam');
-    console.log('nav-param', this.note);
   }
 
   ionViewDidLoad() {
